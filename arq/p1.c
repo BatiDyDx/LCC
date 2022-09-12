e32dd5b457b2fb12931731230598151927367ae2
Bauti Peirone <bautista.peirone@gmail.com>
2022-09-11 23:48:25 -0300

    Cambios generales

--- arq/p1.c
+++ arq/p1.c
@@ -13,10 +13,12 @@ void ex1() {
   assert(0b00000000000000000000000000000000 == 0x80000000 + 0x80000000);
 }
 
+// Ejercicio 2
 int is_one(long n, int b) {
   return (n >> b) & 1;
 }
 
+// Ejercicio 3 
 void printbin(unsigned long n) {
   for (int i = sizeof(unsigned long) * 8; i > 0; i--)
   	putchar(is_one(n, i - 1) ? '1': '0');
@@ -26,12 +28,10 @@ void printbin(unsigned long n) {
 // Ejercicio 4
 void swap3(int *a, int *b, int *c) {
  // a -> b, b -> c, c -> a
- *a = *a ^ *b; // a = a^b
- *b = *a ^ *b; // b = (a^b)^b == a
- *a = *a ^ *b; // a = (a^b)^a == b
- *a = *a ^ *c; // a = b^c
- *c = *a ^ *c; // c = (b^c)^c == b
- *a = *a ^ *c; // a = (b^c)^b == c
+ *a = *a ^ *b ^ *c;
+ *b = *a ^ *b ^ *c; // b = ( a ^ b ^ c ) ^ b ^ c == a
+ *c = *a ^ *b ^ *c; // c = ( a ^ b ^ c ) ^ a ^ c == b
+ *a = *a ^ *b ^ *c; // a = ( a ^ b ^ c ) ^ a ^ b == c
 }
 
 // Ejercicio 5
@@ -85,7 +85,7 @@ int is_even_256b(uint_256bits x) {
   return x.n[0] & 0;
 }
 
-uint_256bits shiftl(uint_256bits x, int n) {
+uint_256bits lshift(uint_256bits x, int n) {
   uint_256bits y;
 
   int block_size = sizeof(short) * 8;
@@ -96,13 +96,12 @@ uint_256bits shiftl(uint_256bits x, int n) {
     if (i - offset < 0) {
       y.n[i] = 0;
 	  continue;
-	}
+	  }
 
-	y.n[i] = (x.n[i - offset] << nmod);
-	if (i - offset - 1 >= 0)
-	  y.n[i] = y.n[i] | x.n[i - offset - 1] >> (block_size - nmod);
-  }
-
+	  y.n[i] = (x.n[i - offset] << nmod);
+	  if (i - offset - 1 >= 0)
+	    y.n[i] = y.n[i] | x.n[i - offset - 1] >> (block_size - nmod);
+    }
   return y;
 }
 
@@ -114,16 +113,17 @@ uint_256bits rshift(uint_256bits x, int n) {
   int nmod = n % block_size;
 
   for (int i = 0; i < 16; ++i) {
-    if (i + offset >= 16 ) {
+    if (i + offset >= 16) {
       y.n[i] = 0;
 	  continue;
-	}
+	  }
 
 	y.n[i] = (x.n[i + offset] >> nmod);
 	if (i + offset + 1 <= 15)
 	  y.n[i] = y.n[i] | x.n[i + offset + 1] << (block_size - nmod);
-
-  return y;
+  }
+  
+	return y;
 }
 
 uint_256bits add_256b(uint_256bits x, uint_256bits y) {
@@ -131,21 +131,58 @@ uint_256bits add_256b(uint_256bits x, uint_256bits y) {
   uint_256bits z;
   for (int i = 0; i < 16; ++i) {
     tmp = (unsigned) x.n[i] + (unsigned) y.n[i] + carry;
-	z.n[i] = (unsigned short) tmp;
-	carry = (tmp >> 16) & 1;
+	  z.n[i] = (unsigned short) tmp;
+	  carry = (tmp >> 16) & 1;
   }
 
   return z;
 }
 
+// Ejercicio 8
+uint_256bits mult_256b(uint_256bits x, uint_256bits y) {
+  int stop = 0;
+	uint_256bits z;
+  
+  if (is_zero_256b(y))
+		for (int i = 0; i < 16; z.n[i++] = 0);
+  else if (is_one_256b(y)) {
+		for (int i = 0; i < 16; z.n[i] = x.n[i], i++);
+	} else {
+    z = mult_256b(lshift(x, 1), rshift(y, 1));
+		if (!is_even_256b(y))
+			z = add_256b(z, x);
+  }
+  return z;
+}
 
 // Ejercicio 9
-#define MANT_F(X) (*( (int*) (X) ) & 0x007FFFFF) // Mantisa para float
-#define EXP_F(X) ((*( (int^*) (X)) >> 23) & 0x0000007F) // Exponente para float
+#define MANT_F(X) (*( (int*) (&X) ) & 0x007FFFFF) // Mantisa para float
+#define EXP_F(X) ((*( (int*) (&X)) >> 23) & 0x000000FF) // Exponente para float
+
+// Ejercicio 10
+// d)
+int myisnan(float f) {
+  return EXP_F(f) == 0xFF && MANT_F(f) != 0;
+}
+
+// f)
+int myisinf(float f) {
+  return EXP_F(f) == 0xFF && MANT_F(f) == 0;
+}
+
+void test_uint256bits() {
+  uint_256bits x, y, z;
+  x.n[0] = 0xFFFF;
+	y.n[0] = 0x0001;
+	z = add_256b(x, y);
+	assert(z.n[0] == 0 && z.n[1] == 1);
+}
 
 int main() {
   ex1();
-  uint_256bits x = { {1, } };
-  printf("%d\n", shiftl(x, 9).n[1]);
-  return 0;
+  test_uint256bits();
+	assert(myisinf(1.0 / 0.0));
+  assert(myisinf(-1.0 / 0.0));
+  assert(myisnan(0.0 / 0.0));
+	return 0;
 }
