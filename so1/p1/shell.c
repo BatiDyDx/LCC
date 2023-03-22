e7df6fb1d8f19851fced1fb178c1f7e4b22b94a5
Bauti Peirone <bautista.peirone@gmail.com>
2023-03-21 22:24:15 -0300

    Cambios a la shell

--- so1/p1/shell.c
+++ so1/p1/shell.c
@@ -1,4 +1,4 @@
-#include <sys/fcntl.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,27 +7,58 @@
 #include <assert.h>
 #include <fcntl.h>
 
-typedef struct {
-  char** args;      // Array of strings representing commands, arguments, pipes, redirections, etc
-  unsigned len;     // Length of args array
-  char* redir_path; // Path to redirect output of command, NULL if no redirection is specified
-  unsigned* pipes;  // Positions in args where there is a pipe. Parse replaces this strings by a NULL pointer
-  char npipes;      // Number of pipes in command, equivalent to length of pipes array
-} Cmd;
+typedef struct _A_Cmd {
+  char** toks;
+  unsigned len;
+  char* redir_path; // Path to redirect output of command, NULL if no redirection is specified;
+} *Atomic_Cmd;
 
-#define CMD_IS_EXIT(cmd) (cmd.args[0] && strcmp(cmd.args[0], EXIT_CMD) == 0)
+typedef struct _Cmd {
+  Atomic_Cmd* atoms; // Atomic commands 
+  unsigned char nprocs; // Number of processes (equal to length of atoms array)
+} *Cmd;
+
+#define ATOMS_SIZE 3
+#define TOKS_SIZE 5
+
 #define TOKEN_IS_PIPE(tok) (tok != NULL && !strcmp(tok, "|"))
 #define TOKEN_IS_REDIR(tok) (tok != NULL && !strcmp(tok, ">"))
-#define ABORT(msg) {fprintf(stderr, "[ERROR]: %s\n", msg); exit(EXIT_FAILURE);}
+#define ABORT(msg) do {\
+            fprintf(stderr, "[ERROR]: %s\n", msg);\
+            exit(EXIT_FAILURE);\
+          } while (0)
 
-// This macros do not check bounds on accessing the array
+#define INFO(msg) do {\
+            fprintf(stderr, "[INFO]: %s\n", msg);\
+            exit(EXIT_SUCCESS);\
+          } while (0)
+
+// These macros do not check bounds on accessing the array
 #define READ_END(filedes, i) filedes[2 * (i)]
 #define WRITE_END(filedes, i) filedes[2 * (i) + 1]
 
-const char* const EXIT_CMD = "exit";
+const char* const EXIT_TOKEN = "exit";
 
 void show_prompt() {
   printf("> ");
+}
+
+Atomic_Cmd create_atomcmd(char** toks, unsigned len) {
+  Atomic_Cmd atom = malloc(sizeof(struct _A_Cmd));
+  assert(atom != NULL);
+
+  if (len >= 3 && TOKEN_IS_REDIR(toks[len - 2])) {
+    toks[len - 2] = NULL;
+    atom->redir_path = toks[len - 1];
+    len -= 2; // Shorten the args list up to the last argumen before >
+  } else {
+    toks[len] = NULL;
+    atom->redir_path = NULL;
+  }
+  atom->toks = toks;
+  atom->len = len;
+
+  return atom;
 }
 
 char* get_input() {
@@ -48,59 +79,71 @@ char* get_input() {
   return input;
 }
 
+void add_atomic(Cmd cmd, Atomic_Cmd atom) {
+  if (cmd->nprocs % ATOMS_SIZE == 0) {
+    cmd->atoms = realloc(cmd->atoms, sizeof(Atomic_Cmd) * (cmd->nprocs + ATOMS_SIZE));
+    assert(cmd->atoms != NULL);
+  }
+  cmd->atoms[cmd->nprocs++] = atom;
+}
+
 Cmd parse(char* input) {
-  size_t size_args = 10, size_pipes = 9;
-  char** args = malloc(sizeof(char*) * (size_args + 1));
-  unsigned npipes = 0, *pipes = malloc(sizeof(unsigned) * size_pipes);
+  size_t toks_len = 1;
+  Atomic_Cmd atom;
+  Cmd cmd = malloc(sizeof(struct _Cmd));
+  assert(cmd != NULL);
+
+  cmd->nprocs = 0;
+  cmd->atoms = malloc(sizeof(Atomic_Cmd) * ATOMS_SIZE);
+  assert(cmd->atoms != NULL);
+
+  char** toks = malloc(sizeof(char*) * (TOKS_SIZE + 1));
   char* tmp;
   char* delim = " \n\t";
 
-  args[0] = strtok(input, delim);
-  int i;
-  for (i = 1; (tmp = strtok(NULL, delim)); ++i) {
-    if (i == size_args) {
-      size_args += 10;
-      args = realloc(args, sizeof(char*) * (size_args + 1));
-    }
-    args[i] = tmp;
+  toks[0] = strtok(input, delim);
+  while ((tmp = strtok(NULL, delim))) {
+    if (toks_len % TOKS_SIZE == 0)
+      toks = realloc(toks, sizeof(char*) * (toks_len + TOKS_SIZE + 1));
+    toks[toks_len++] = tmp;
     if (TOKEN_IS_PIPE(tmp)) {
-      if (npipes == size_pipes) {
-        size_pipes += 2;
-        pipes = realloc(pipes, sizeof(unsigned) * size_pipes);
-        assert(pipes != NULL);
-      }
-      pipes[npipes++] = i;
-      args[i] = NULL; // Replace the "|" by a NULL, this lets us treat the args array as multiple
-                      // commands by saving the positions of the pipes
+      atom = create_atomcmd(toks, toks_len - 1);
+      add_atomic(cmd, atom);
+      toks = malloc(sizeof(char*) * (TOKS_SIZE + 1));
+      assert(toks != NULL);
+      toks_len = 0;
     }
   }
-  args[i] = NULL;
 
-  int len = i;
-  char* redir_path = NULL;
-  if (i >= 3 && TOKEN_IS_REDIR(args[i-2])) {
-    redir_path = args[i-1];
-    args[i-2] = NULL; // Shorten the args list up to the last argumen before >
-    len = i - 2;
-  }
-  Cmd cmd = { args, len, redir_path, pipes, npipes };
+  atom = create_atomcmd(toks, toks_len);
+  add_atomic(cmd, atom);
 
   return cmd;
 }
 
-void handle_cmd(char** cmd, char* redir_path) {
-  if (redir_path != NULL) {
+int cmd_is_exit(Cmd cmd) {
+  // TODO Improve exit condition
+  return cmd->nprocs > 0 && cmd->atoms[0]->len > 0 &&
+          !strcmp(cmd->atoms[0]->toks[0], EXIT_TOKEN);
+}
+
+void exec_atomic(Atomic_Cmd acmd) {
+  if (acmd->redir_path != NULL) {
     close(STDOUT_FILENO);
-    open(redir_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    open(acmd->redir_path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
   }
-  execvp(cmd[0], cmd);
+  execvp(acmd->toks[0], acmd->toks);
   ABORT("Command not found");
 }
 
-char** ith_process_cmd(Cmd cmd, unsigned i) {
-  if (i == 0) return cmd.args;
-  int last_pipe = cmd.pipes[i - 1];
-  return cmd.args + last_pipe + 1;
+void free_atomic(Atomic_Cmd ac) {
+  free(ac->toks);
+  free(ac);
+}
+
+void free_cmd(Cmd cmd) {
+  for (int i = 0; i < cmd->nprocs; free_atomic(cmd->atoms[i++]));
+  free(cmd);
 }
 
 int main() {
@@ -109,61 +152,52 @@ int main() {
     char* input = get_input();
     Cmd cmd = parse(input);
 
-    if (CMD_IS_EXIT(cmd))
-      break;
-    int* filedes = malloc(sizeof(int) * 2 * cmd.npipes);
+    if (cmd_is_exit(cmd))
+      INFO("Exiting terminal");
+
+    int npipes = cmd->nprocs - 1;
+
+    int* filedes = malloc(sizeof(int) * 2 * (cmd->nprocs - 1));
     assert(filedes != NULL);
 
-    for (int i = 0; i < cmd.npipes; i++)
-      pipe(filedes + i * 2);
+    for (int i = 0; i < npipes; i++) {
+      if (pipe(filedes + i * 2) < 0)
+        ABORT("Could not create pipe");
+    }
 
-    int nprocs = cmd.npipes + 1;
-    pid_t* pids = malloc(sizeof(pid_t) * nprocs);
+    pid_t* pids = malloc(sizeof(pid_t) * cmd->nprocs);
     assert(pids != NULL);
 
-    // printf("npipes: %d, len: %d, positions: ", cmd.npipes, cmd.len);
-    // for (int i = 0; i < cmd.npipes; i++)
-    //   printf("%u ", cmd.pipes[i]);
-    // putchar('\n');
-    // for (int i = 0, j = 0; j < cmd.npipes + 1; i++) {
-    //   printf("%s, ", cmd.args[i] == NULL ? "NULL" : cmd.args[i]);
-    //   if (cmd.args[i] == NULL) j++;
-    // }
-    //
-    // fflush(stdout);
-    for (int i = 0; i < nprocs; i++) {
-      char* redir_path = NULL;
+    for (int i = 0; i < cmd->nprocs; i++) {
       pids[i] = fork();
-      if (pids[i] < 0) {
+      if (pids[i] < 0)
         ABORT("Could not create child process");
-      } else if (pids[i] == 0) {
+      else if (pids[i] == 0) {
         if (i != 0) {
           close(WRITE_END(filedes, i - 1));
           dup2(READ_END(filedes, i - 1), STDIN_FILENO);
         }
-        if (i != nprocs - 1) {
+        if (i != cmd->nprocs - 1) {
           close(READ_END(filedes, i));
           dup2(WRITE_END(filedes, i), STDOUT_FILENO);
         }
-        for (int j = 0; j < 2 * cmd.npipes; j++)
+        for (int j = 0; j < 2 * npipes; j++)
           close(filedes[j]);
-        if (i == nprocs - 1) // If this is the last process of the pipe, check if output is redirected
-          redir_path = cmd.redir_path;
-        handle_cmd(ith_process_cmd(cmd, i), redir_path);
+        exec_atomic(cmd->atoms[i]);
       }
     }
 
-    for (int j = 0; j < 2 * cmd.npipes; j++)
+    for (int j = 0; j < 2 * npipes; j++)
       close(filedes[j]);
 
     // TODO use static memory to avoid memory leaking
     free(input);
-    free(cmd.args);
     free(filedes);
 
-    int status;
-    for (int i = 0; i < nprocs; i++)
-      waitpid(pids[i], &status, 0);
+    for (int i = 0; i < cmd->nprocs; i++)
+      waitpid(pids[i], NULL, 0);
+
+    free_cmd(cmd);
     free(pids);
     // TODO check wait status of the commands
   }
