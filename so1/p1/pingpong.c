ac5a6abba1cc7af9a84880f67c2f91efa354234c
Bauti Peirone <bautista.peirone@gmail.com>
2023-04-05 00:34:07 -0300

    Antes de merge

--- so1/p1/pingpong.c
+++ so1/p1/pingpong.c
@@ -8,48 +8,42 @@
 int flag[2];
 
 void handler(int sig) {
-    if (sig == SIGUSR1) {
-        flag[PARENT] = 1;
-    } else {
-        flag[CHILD] = 1;
-    }   
-    return;
+  if (sig == SIGUSR1) {
+    flag[PARENT] = 1;
+  } else if (sig == SIGUSR2) {
+    flag[CHILD] = 1;
+  }
 }
 
 void child_process() {
-    while (1) {
-        printf("Ping!\n");
-        kill(getppid(), SIGUSR2);
-        while (flag[PARENT] == 0) { }
-    }
-    return;
+  while (1) {
+    printf("Ping!\n");
+    kill(getppid(), SIGUSR2);
+    while (flag[PARENT] == 0);
+  }
 }
 
 void parent_process(pid_t t) {
-    while (1) {
-        //kill(t, SIGUSR1);
-        while (flag[CHILD] == 0) { }
-        printf("Pong!\n");
-
-        kill(t, SIGUSR1);
-    }
-    return;
+  while (1) {
+    while (flag[CHILD] == 0);
+    printf("Pong!\n");
+    kill(t, SIGUSR1);
+  }
 }
 
 void setup() {
-    flag[CHILD] = 0;
-    flag[PARENT] = 0;
-    signal(SIGUSR1, handler);
-    signal(SIGUSR2, handler);
+  flag[CHILD] = 0;
+  flag[PARENT] = 0;
+  signal(SIGUSR1, handler);
+  signal(SIGUSR2, handler);
 }
 
-int main(int argc, char* argv[]) {  
-    setup();
-    pid_t t = fork();
-    if (t == 0) {
-        child_process();
-    } else {
-        parent_process(t);
-    }
-    return 0;
+int main() {  
+  setup();
+  pid_t t = fork();
+  if (t == 0)
+    child_process();
+  else
+    parent_process(t);
+  return 0;
 }
